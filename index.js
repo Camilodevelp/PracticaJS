beb8ccfff221bf15a1f8d2c42db387f5c5d29f24
camilo <kmiloeduardo@gmail.com>
2023-05-31 07:34:46 -0500

    subida de archivos de js y html

---
+++ index.js
@@ -0,0 +1,23 @@
+let operacion = prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir")
+let A = prompt ("Otorga el primer numero de la operación")
+let B = prompt ("Otorga el segundo numero de la operación")
+
+while (operacion != 5){
+
+    switch (operacion) {
+        case 1:
+            console.log = (A + B)
+            break;
+        case 2:
+            (A - B)
+            break;
+        case 3:
+            (A * B)
+            break;
+        case 4:
+            (A / B)
+            break;
+            
+                        
+    }
+}
