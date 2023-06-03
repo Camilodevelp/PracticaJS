4a92ce7411033aaccaf72d251c0f39a67345333a
camilo <kmiloeduardo@gmail.com>
2023-06-02 19:08:30 -0500

    mejorando codigo

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+console.log("prueba")
 let operacion = prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir")
 let A = prompt ("Otorga el primer numero de la operación")
 let B = prompt ("Otorga el segundo numero de la operación")
 
 while (operacion != 5){
-
-    switch (operacion) {
-        case 1:
-            console.log = (A + B)
-            break;
-        case 2:
-            (A - B)
-            break;
-        case 3:
-            (A * B)
-            break;
-        case 4:
-            (A / B)
-            break;
-            
-                        
+    function resultado (){        
+        switch (operacion) {
+            case 1:
+                return  (resultado = A + B)
+                break;
+            case 2:
+                return (resultado = A - B)
+                break;
+            case 3:
+                return (resultado = A * B)
+                break;
+            case 4:
+               return (resultado = A / B)
+                break;
+                            
+                            
+         }
     }
-}
+}     
+alert(resultado)
