6cb479083bcbd5610c54b545eedbc09deafa019a
camilo <kmiloeduardo@gmail.com>
2023-06-02 21:14:52 -0500

    entrega final

--- index.js
+++ index.js
@@ -1,81 +1,41 @@
+let operacion = parseInt(prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir"));
+let A;
+let B;
 
-// let operacion = parseInt(prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir"));
-// let A;
-// let B;
+if (operacion != 5) {
+	A = parseInt(prompt("Otorga el primer numero de la operación"));
+	B = parseInt(prompt("Otorga el segundo numero de la operación"));
+}
 
-// if(operacion != 5){
-//     A = parseInt(prompt ("Otorga el primer numero de la operación"));
-//     B = parseInt(prompt ("Otorga el segundo numero de la operación"));
-// }
+let resultado = 0;
 
-// let resultado = 0;
+function realizarCuenta(operacion) {
+	switch (operacion) {
+		case 1:
+			resultado = A + B;
+			alert(resultado);
+			break;
+		case 2:
+			resultado = A - B;
+			alert(resultado);
+			break;
+		case 3:
+			resultado = A * B;
+			alert(resultado);
+			break;
+		case 4:
+			resultado = A / B;
+			alert(resultado);
+			break;
+	}
+}
 
-// while (operacion != 5){
-//         switch (operacion) {
-//             case 1:
-//                  (resultado = A + B)
-//                  alert(resultado)
-//                 break;
-//             case 2:
-//                  (resultado = A - B)
-//                  alert(resultado)
-//                 break;
-//             case 3:
-//                  (resultado = A * B)
-//                  alert(resultado)
-//                 break;
-//             case 4:
-//                 (resultado = A / B)
-//                  alert(resultado)
-//                  break;
-//          }
-//          operacion = parseInt(prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir"));
+while (operacion != 5) {
+	realizarCuenta(operacion);
+	operacion = parseInt(prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir"));
 
-//          if(operacion != 5){
-//             A = parseInt(prompt ("Otorga el primer numero de la operación"));
-//             B = parseInt(prompt ("Otorga el segundo numero de la operación"));
-//         }
-            
-// }     
-
-
-let operacion =0;
-let A =0;
-let B =0;
-let resultado=0;
-do {
-
-operacion = parseInt(prompt("elige la operacion que deseas, \n 1- suma \n 2 resta \n 3 multiplicacion \n 4 division \n 5 salir"));
-A =0;
-B =0;
-  switch (operacion) {
-            case 1:
-			 A = parseInt(prompt ("Otorga el primer numero de la operación"));
-             B = parseInt(prompt ("Otorga el segundo numero de la operación"));
-                 (resultado = A + B)
-                 alert(resultado)
-                break;
-            case 2:
-			   A = parseInt(prompt ("Otorga el primer numero de la operación"));
-               B = parseInt(prompt ("Otorga el segundo numero de la operación"));
-                 (resultado = A - B)
-                 alert(resultado)
-                break;
-            case 3:
-			   A = parseInt(prompt ("Otorga el primer numero de la operación"));
-               B = parseInt(prompt ("Otorga el segundo numero de la operación"));
-                 (resultado = A * B)
-                 alert(resultado)
-                break;
-            case 4:
-			   A = parseInt(prompt ("Otorga el primer numero de la operación"));
-               B = parseInt(prompt ("Otorga el segundo numero de la operación"));
-                (resultado = A / B)
-                 alert(resultado)
-                 break;
-			default:
-			    break;
-         }
-
-} while (operacion != 5);
-console.log("termino");
+	if (operacion != 5) {
+		A = parseInt(prompt("Otorga el primer numero de la operación"));
+		B = parseInt(prompt("Otorga el segundo numero de la operación"));
+	}
+}
